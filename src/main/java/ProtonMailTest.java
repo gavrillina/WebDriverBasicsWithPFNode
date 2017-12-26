ffde162d44ed7f76ea176e49477c067316d5903a
John Doe <johndoe@example.com>
2017-12-26 17:13:05 +0600

    add Node

--- src/main/java/ProtonMailTest.java
+++ src/main/java/ProtonMailTest.java
@@ -1,4 +1,6 @@
 import entity.Mail;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import page.*;
 import exeptions.CannotLoginException;
 import exeptions.DraftNotFoundException;
@@ -10,6 +12,10 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 
 public class ProtonMailTest {
     WebDriver driver;
@@ -18,11 +24,20 @@ public class ProtonMailTest {
 
     @BeforeClass
     private void openBrauser() {
-
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+        try {
+            driver = new RemoteWebDriver(new URL("http://10.12.12.191:4444/wd/hub"), DesiredCapabilities.chrome());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://protonmail.com/");
         driver.manage().window().maximize();
+
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+//        driver = new ChromeDriver();
+//
+//        driver.manage().window().maximize();
     }
 
 
